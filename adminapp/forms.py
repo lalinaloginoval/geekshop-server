b062ba3111a0360c443686f2391e244277262635
lalinaloginoval <alina.login30@gmail.com>
2021-05-21 14:26:22 +0300

    added update/remove user

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 
-from authapp.forms import UserRegisterForm
+from authapp.forms import UserRegisterForm, UserProfileForm
 from authapp.models import User
+
 
 class UserAdminRegisterForm(UserRegisterForm):
     image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
@@ -9,3 +10,8 @@ class UserAdminRegisterForm(UserRegisterForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'image', 'first_name', 'last_name', 'password1', 'password2')
+
+
+class UserAdminProfileForm(UserProfileForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control py-4'}))
