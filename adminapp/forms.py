ca36cf7ac9e85e4f63a2f82f3548782be35c51f0
lalinaloginoval <alina.login30@gmail.com>
2021-05-21 13:40:21 +0300

    added create user

---
+++ adminapp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+from authapp.forms import UserRegisterForm
+from authapp.models import User
+
+class UserAdminRegisterForm(UserRegisterForm):
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'image', 'first_name', 'last_name', 'password1', 'password2')
