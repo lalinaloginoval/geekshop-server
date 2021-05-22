b895031ed12e154bfcc57b4e7b668d52f077c55a
lalinaloginoval <alina.login30@gmail.com>
2021-05-22 11:24:25 +0300

    added operation for products

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 from authapp.forms import UserRegisterForm, UserProfileForm
 from authapp.models import User
+from mainapp.models import Product, ProductCategory
 
 
 class UserAdminRegisterForm(UserRegisterForm):
@@ -15,3 +16,21 @@ class UserAdminRegisterForm(UserRegisterForm):
 class UserAdminProfileForm(UserProfileForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
     email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control py-4'}))
+
+
+class ProductAdminForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control', 'placeholder': 'Введите название продукта'}))
+    category = forms.IntegerField(widget=forms.Select(attrs={
+        'class': 'form-control'}, choices=[(c.id, c.name) for c in ProductCategory.objects.all()]))
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
+    price = forms.DecimalField(widget=forms.TextInput(attrs={
+        'class': 'form-control', 'placeholder': 'Введите стоимость', 'type': 'number', 'min': '0'}))
+    quantity = forms.IntegerField(widget=forms.TextInput(attrs={
+        'class': 'form-control', 'placeholder': 'Введите количество', 'type': 'number', 'min': '0'}))
+    description = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control', 'placeholder': 'Введите описание'}))
+
+    class Meta:
+        model = Product
+        fields = ('name', 'category', 'image', 'price', 'quantity', 'description')
--- adminapp/views.py
+++ adminapp/views.py
@@ -3,7 +3,8 @@ from django.urls import reverse
 from django.contrib.auth.decorators import user_passes_test
 
 from authapp.models import User
-from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
+from mainapp.models import Product, ProductCategory
+from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm, ProductAdminForm
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -50,3 +51,51 @@ def admin_users_remove(request, user_id):
     user.is_active = False
     user.save()
     return HttpResponseRedirect(reverse('admin_staff:admin_users_read'))
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_products_read(request):
+    context = {'products': Product.objects.all()}
+    return render(request, 'adminapp/admin-products-read.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_products_create(request):
+    if request.method == 'POST':
+        form = ProductAdminForm(data=request.POST, files=request.FILES)
+
+        category = ProductCategory.objects.get(id=request.POST['category'])
+        form.instance.category_id = category.id
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admin_staff:admin_products_read'))
+    else:
+        form = ProductAdminForm()
+    context = {'form': form}
+    return render(request, 'adminapp/admin-products-create.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_products_update(request, product_id):
+    selected_product = Product.objects.get(id=product_id)
+    if request.method == 'POST':
+        form = ProductAdminForm(data=request.POST, files=request.FILES, instance=selected_product)
+
+        category = ProductCategory.objects.get(id=request.POST['category'])
+        form.instance.category_id = category.id
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admin_staff:admin_products_read'))
+    else:
+        form = ProductAdminForm(instance=selected_product)
+    context = {'form': form, 'selected_product': selected_product}
+    return render(request, 'adminapp/admin-products-update-delete.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_products_remove(request, product_id):
+    product = Product.objects.get(id=product_id)
+    product.delete()
+    return HttpResponseRedirect(reverse('admin_staff:admin_products_read'))
