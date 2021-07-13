9f60038dfbd62704bd7aa5abaee8e984e99f2288
lalinaloginoval <70517164+lalinaloginoval@users.noreply.github.com>
2021-07-13 15:56:59 +0300

    fix

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-b0(9icgm)hn5z(zerfr*8xxiebry&-efh+58%gl(&zs46)gjv4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -88,9 +88,15 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'geekshop',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'django',
     }
 }
 
